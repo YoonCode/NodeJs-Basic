c3d239ca12e677e5735ddf83b29c551bdc276fc0
YoonCode <yjin830201@gmail.com>
2022-05-30 00:01:36 +0900

    request, response

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const http = require('http')
 // 3. Arrow 함수 사용
 // http.createServer((req, res) => {})
 const server = http.createServer((req, res) => {
-  console.log(req)
+  // console.log(req)
+  console.log(req.url, req.method, req.headers)
+  // process.exit() // 서버 종료
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+  res.write('</html>')
+  res.end()
 }) // 생성된 서버를 상수로 저장
 
 server.listen(3000)
