dade29e16e5fdc2976805aabe6010f7e794b89b0
YoonCode <yjin830201@gmail.com>
2022-05-31 23:03:19 +0900

    module.exports

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  const url = req.url
-  const method = req.method
-  if (url === '/') {
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title><head>')
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>',
-    )
-    res.write('</html>')
-    return res.end()
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk)
-    })
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-      })
-    })
-  }
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>My First Page</title><head>')
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
-  res.write('</html>')
-  res.end()
-})
+const routes = require('./routes') // 1. 같은 폴더 안에 있는 routes.js 를 찾을 것
+
+const server = http.createServer(routes) // 3. routes.js 의 requestHandler 를 사용할 수 있음
+
+// console.log(routes.someText)
+
+// const server = http.createServer(routes.handler)
 
 server.listen(3000)
